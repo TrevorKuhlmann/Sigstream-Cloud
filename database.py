ad4d012502ed194d8e426482b871f57a270c6cdc
Trevor <trevorkuhlmannk@gmail.com>
2025-05-07 22:36:37 +0200

    recmomit

--- database.py
+++ database.py
@@ -8,4 +8,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-Base.metadata.create_all(bind=engine) ####TEMP
+
+# Only use this during initial schema setup
+# Base.metadata.create_all(bind=engine)
