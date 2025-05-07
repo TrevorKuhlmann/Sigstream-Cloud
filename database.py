06fce498174d9a0506cc12946c7b50b944829f9c
Trevor <trevorkuhlmannk@gmail.com>
2025-05-07 22:50:22 +0200

    recommit

--- database.py
+++ database.py
@@ -3,6 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+from database import Base, engine
+import models
+
+# TEMP: Ensure all tables exist in Postgres (one-time)
+Base.metadata.create_all(bind=engine)
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
@@ -10,4 +17,4 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # Only use this during initial schema setup
-# Base.metadata.create_all(bind=engine)
+
