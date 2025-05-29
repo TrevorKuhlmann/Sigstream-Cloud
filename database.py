29667b4d321091f93b6bb82f6973f46b57160fd0
Trevor <trevorkuhlmannk@gmail.com>
2025-05-29 07:57:33 +0200

    fixed database.py

--- database.py
+++ database.py
@@ -1,10 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -12,6 +11,14 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Dependency to get DB session
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # Only use this during initial schema setup
 # Base.metadata.create_all(bind=engine)
